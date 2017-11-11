75d680981b0e4f2b4e63827fa9e5b83393538dae
Alexsander Falcucci <alex.falcucci@gmail.com>
2017-11-11 06:18:29 -0200

    Just committing so I can go home

---
+++ src/App-class-13.js
@@ -0,0 +1,48 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      increasing: false
+    }
+  }
+
+  update(e) {
+    ReactDOM.render(
+      <App val={this.props.val+1}></App>,
+      document.getElementById('root')
+    )
+  }
+
+  componentWillReceiveProps(nextProps) {
+   this.setState({
+     increasing: nextProps.val > this.props.val
+   });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.val % 5 == 0;
+  }
+
+  render() {
+    console.log(this.state.increasing);
+    return (
+      <button onClick={this.update.bind(this)}>
+        {this.props.val}
+      </button>
+    );
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(`prevProps: ${prevProps.val}`);
+  }
+}
+
+App.defaultProps = {
+  val: 0
+};
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
   render() {
     return (
-      <h1>Hey there</h1>
+      <h1>hi</h1>
     );
   }
 }
