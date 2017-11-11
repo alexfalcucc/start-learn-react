904aaa5a0d3f2804b2f126990607bb467113480f
Alexsander Falcucci <alex.falcucci@gmail.com>
2017-11-11 06:16:39 -0200

    Replace all whitespaces with tabs.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component} from 'react';
 import PropTypes from 'prop-types';
-import styles from './App.css';
 
 class App extends Component {
   static propTypes = {
@@ -14,9 +13,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className={styles.base}>
-
-      </div>
+      <h1>Hey there</h1>
     );
   }
 }
